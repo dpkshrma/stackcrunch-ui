f0ba31e4d4857a8783594ac2907aac5916c3ec07
Deepak Sharma <dpkshrma01@gmail.com>
2017-10-22 16:29:37 +0530

    Check if user exists using input debounce

--- src/components/Join/index.js
+++ src/components/Join/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
+import { DebounceInput } from 'react-debounce-input';
 import queryString from 'query-string';
 import StackExchangeIcon from './icons/stackexchange';
 import GithubIcon from './icons/github';
@@ -24,17 +25,19 @@ const Title = styled.span`
   letter-spacing: 2px;
   text-transform: uppercase;
 `;
-const Input = styled.input`
+const Input = styled(DebounceInput)`
   font-size: 14px;
   outline: none;
   border: 1px solid #ccc;
   padding: 16px;
   width: calc(100% - 32px);
   border-radius: 4px;
+  margin-bottom: 4px;
   &:active,
   &:focus {
     border-color: #3af;
   }
+  ${({ css }) => css};
 `;
 const ButtonGroup = styled.div`
   display: flex;
@@ -80,37 +83,114 @@ const twCSS = css`
   margin-right: 0;
   margin-left: 8px;
 `;
+const unameExistsInputCSS = css`
+  &,
+  &:active,
+  &:focus {
+    border-color: #f44336;
+  }
+`;
+const unameAvailableInputCSS = css`
+  &,
+  &:active,
+  &:focus {
+    border-color: #50a024;
+  }
+`;
 
-export default props => {
-  const { tab } = queryString.parse(props.location.search);
-  const joinButtons = (
-    <ButtonGroup>
-      <Button css={seCSS}>
-        <StackExchangeIcon height="32" />
-      </Button>
-      <Button css={ghCSS}>
-        <GithubIcon height="32" />
-      </Button>
-      <Button css={twCSS}>
-        <TwitterIcon height="24" />
-      </Button>
-    </ButtonGroup>
-  );
-  return (
-    <Wrapper>
-      <Title>{tab === 'signup' ? 'Sign Up' : 'Sign In'}</Title>
-      {tab === 'signup' && <Input type="text" placeholder="Username" />}
-      {joinButtons}
-      <Text>
-        {tab === 'signup'
-          ? 'Already have an account?'
-          : "Don't have an account yet?"}
-      </Text>
-      {tab === 'signup' ? (
-        <JoinLink to={'/join?tab=signin'}>SignIn</JoinLink>
-      ) : (
+export default class Join extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      uname: '',
+      unameChecked: false,
+      unameExists: false
+    };
+  }
+  onUsernameChange = e => {
+    const uname = e.target.value;
+    if (uname.length < 3) {
+      this.setState({
+        uname,
+        unameChecked: false,
+        unameExists: false
+      });
+    } else {
+      this.setState(
+        {
+          uname
+        },
+        () => {
+          // api call to check whether username exists
+          setTimeout(() => {
+            this.setState({
+              unameChecked: true,
+              unameExists: false
+            });
+          }, 1000);
+        }
+      );
+    }
+  };
+  render() {
+    const { tab } = queryString.parse(this.props.location.search);
+
+    let unameInputCSS;
+    if (this.state.unameChecked) {
+      if (this.state.unameExists) {
+        unameInputCSS = unameExistsInputCSS;
+      } else {
+        unameInputCSS = unameAvailableInputCSS;
+      }
+    }
+
+    let inputMsg;
+    if (this.state.unameChecked) {
+      if (this.state.unameExists) {
+        inputMsg = <Text>Username already exists.</Text>;
+      } else {
+        inputMsg = <Text>Cool! This username is available!</Text>;
+      }
+    }
+    const joinButtons = (
+      <ButtonGroup>
+        <Button css={seCSS}>
+          <StackExchangeIcon height="32" />
+        </Button>
+        <Button css={ghCSS}>
+          <GithubIcon height="32" />
+        </Button>
+        <Button css={twCSS}>
+          <TwitterIcon height="24" />
+        </Button>
+      </ButtonGroup>
+    );
+    if (tab === 'signup') {
+      return (
+        <Wrapper>
+          <Title>Sign Up</Title>
+          <Input
+            type="text"
+            placeholder="Username"
+            minLength={3}
+            debounceTimeout={300}
+            onChange={this.onUsernameChange}
+            css={unameInputCSS}
+          />
+          {inputMsg}
+          {joinButtons}
+          <Text>Already have an account?</Text>
+          <JoinLink to={'/join?tab=signin'}>SignIn</JoinLink>
+        </Wrapper>
+      );
+    }
+    return (
+      <Wrapper>
+        <Title>Sign In</Title>
+        {joinButtons}
+        <Text>Don't have an account yet?</Text>
         <JoinLink to={'/join?tab=signup'}>SignUp</JoinLink>
-      )}
-    </Wrapper>
-  );
-};
+      </Wrapper>
+    );
+  }
+}
