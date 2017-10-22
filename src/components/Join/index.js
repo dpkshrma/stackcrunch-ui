469f9f31aee7477d522e64f28b92b118c5936a07
Deepak Sharma <dpkshrma01@gmail.com>
2017-10-22 19:18:39 +0530

    Move Join styled components to separate module

--- src/components/Join/index.js
+++ src/components/Join/index.js
@@ -1,102 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import styled, { css } from 'styled-components';
-import { DebounceInput } from 'react-debounce-input';
 import queryString from 'query-string';
 import StackExchangeIcon from './icons/stackexchange';
 import GithubIcon from './icons/github';
 import TwitterIcon from './icons/twitter';
-
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  min-width: 320px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  padding: 32px;
-  max-height: 300px;
-`;
-const Title = styled.span`
-  font-size: 20px;
-  margin: 12px 0 28px;
-  color: #666;
-  letter-spacing: 2px;
-  text-transform: uppercase;
-`;
-const Input = styled(DebounceInput)`
-  font-size: 14px;
-  outline: none;
-  border: 1px solid #ccc;
-  padding: 16px;
-  width: calc(100% - 32px);
-  border-radius: 4px;
-  margin-bottom: 4px;
-  &:active,
-  &:focus {
-    border-color: #3af;
-  }
-  ${({ css }) => css};
-`;
-const ButtonGroup = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  margin-bottom: 48px;
-`;
-const Button = styled.button`
-  font-size: 24px;
-  color: #444;
-  background: none;
-  border: 1px solid #ccc;
-  margin: 0 8px;
-  cursor: pointer;
-  outline: none;
-  height: 51px;
-  width: 100%;
-  margin-top: 24px;
-  border-radius: 4px;
-  &:hover {
-    background: rgba(0, 0, 0, 0.01);
-  }
-  ${({ css }) => css};
-`;
-const Text = styled.div`
-  font-size: 12px;
-  color: #999;
-`;
-const JoinLink = styled(Link)`
-  text-decoration: none;
-  font-size: 14px;
-  color: #07c;
-`;
-const seCSS = css`
-  margin-left: 0;
-  margin-right: 8px;
-`;
-const ghCSS = css`
-  margin-left: 4px;
-  margin-right: 4px;
-`;
-const twCSS = css`
-  margin-right: 0;
-  margin-left: 8px;
-`;
-const unameExistsInputCSS = css`
-  &,
-  &:active,
-  &:focus {
-    border-color: #f44336;
-  }
-`;
-const unameAvailableInputCSS = css`
-  &,
-  &:active,
-  &:focus {
-    border-color: #50a024;
-  }
-`;
+import {
+  Wrapper,
+  Title,
+  Input,
+  ButtonGroup,
+  Button,
+  Text,
+  JoinLink,
+  seCSS,
+  ghCSS,
+  twCSS,
+  unameExistsInputCSS,
+  unameAvailableInputCSS
+} from './styled';
 
 export default class Join extends React.Component {
   constructor(props) {
---
+++ src/components/Join/styled.js
@@ -0,0 +1,94 @@
+import { Link } from 'react-router-dom';
+import { DebounceInput } from 'react-debounce-input';
+import styled, { css } from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-width: 320px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 32px;
+  max-height: 300px;
+`;
+export const Title = styled.span`
+  font-size: 20px;
+  margin: 12px 0 28px;
+  color: #666;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+`;
+export const Input = styled(DebounceInput)`
+  font-size: 14px;
+  outline: none;
+  border: 1px solid #ccc;
+  padding: 16px;
+  width: calc(100% - 32px);
+  border-radius: 4px;
+  margin-bottom: 4px;
+  &:active,
+  &:focus {
+    border-color: #3af;
+  }
+  ${({ css }) => css};
+`;
+export const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 48px;
+`;
+export const Button = styled.button`
+  font-size: 24px;
+  color: #444;
+  background: none;
+  border: 1px solid #ccc;
+  margin: 0 8px;
+  cursor: pointer;
+  outline: none;
+  height: 51px;
+  width: 100%;
+  margin-top: 24px;
+  border-radius: 4px;
+  &:hover {
+    background: rgba(0, 0, 0, 0.01);
+  }
+  ${({ css }) => css};
+`;
+export const Text = styled.div`
+  font-size: 12px;
+  color: #999;
+`;
+export const JoinLink = styled(Link)`
+  text-decoration: none;
+  font-size: 14px;
+  color: #07c;
+`;
+export const seCSS = css`
+  margin-left: 0;
+  margin-right: 8px;
+`;
+export const ghCSS = css`
+  margin-left: 4px;
+  margin-right: 4px;
+`;
+export const twCSS = css`
+  margin-right: 0;
+  margin-left: 8px;
+`;
+export const unameExistsInputCSS = css`
+  &,
+  &:active,
+  &:focus {
+    border-color: #f44336;
+  }
+`;
+export const unameAvailableInputCSS = css`
+  &,
+  &:active,
+  &:focus {
+    border-color: #50a024;
+  }
+`;
