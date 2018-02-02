5cfe42b6078b6d3b51207226d4efc1dccaadec13
Deepak Sharma <dpkshrma01@gmail.com>
2018-02-02 22:55:34 +0530

    Add editor keyboard commands for b,i,u

--- src/components/PostInput/Editor/index.js
+++ src/components/PostInput/Editor/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Editor } from 'draft-js';
+import { Editor, RichUtils } from 'draft-js';
 import styled from 'styled-components';
 import 'draft-js/dist/Draft.css';
 import Toolbar from './Toolbar';
@@ -16,6 +16,14 @@ class PostEditor extends React.Component {
   onEditorFocus = () => {
     this.setState({ showToolbar: true });
   };
+  handleKeyCommand = (command, editorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.props.onChange(newState);
+      return true;
+    }
+    return false;
+  };
   render() {
     const { editorState, onChange } = this.props;
     return (
@@ -25,6 +33,7 @@ class PostEditor extends React.Component {
           onFocus={this.onEditorFocus}
           placeholder="Write your article here..."
           editorState={editorState}
+          handleKeyCommand={this.handleKeyCommand}
           onChange={onChange}
         />
       </Container>
