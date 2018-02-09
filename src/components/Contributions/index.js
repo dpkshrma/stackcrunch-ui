80a81761b82fc102dad3c4f80a250c45e0dde760
Deepak Sharma <dpkshrma01@gmail.com>
2018-02-09 23:48:26 +0530

    Use infinite list component for contributions

--- src/components/Contributions/index.js
+++ src/components/Contributions/index.js
@@ -1,100 +1,61 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 import { fetchDrafts, fetchUserPublishedPosts } from '../../actions/post';
 import ListItem from '../ListItem';
-import PenIcon from '../icons/Pen';
+import { InfiniteList, Loader as LoaderIcon } from '../common';
+import {
+  Container,
+  List,
+  Heading,
+  LoadMoreButton,
+  LoaderButton,
+  EditButton
+} from './styled';
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-const List = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-right: 16px;
-`;
-const Heading = styled.h2`
-  font-size: 24px;
-  font-family: roboto;
-  font-weight: 300;
-  color: #f78d3d;
-  padding-bottom: 12px;
-  border-bottom: 1px solid #efefef;
-`;
-const EditButton = ({ onClick, to }) => {
-  const Text = styled.span`
-    margin-left: 4px;
-  `;
-  const EditIcon = styled(PenIcon)`
-    fill: #999;
-  `;
-  const Container = styled(Link)`
-    cursor: pointer;
-    display: flex;
-    font-size: 16px;
-    color: #999;
-    margin-left: auto;
-    text-decoration: none;
-    &:hover {
-      ${EditIcon} svg {
-        fill: #777;
-      }
-      ${Text} {
-        color: #777;
-      }
+const renderPost = post => (
+  <ListItem
+    {...post}
+    headerComponent={
+      <ListItem.Header.Container>
+        <ListItem.Header.CreatedOn timeStamp={post.createdOn} />
+        <ListItem.Separator delimiter="|" space={8} />
+        <ListItem.Header.TimeToRead ttr={post.ttr} />
+        <EditButton to={`write/${post.slug}`} />
+      </ListItem.Header.Container>
     }
-  `;
-  return (
-    <Container onClick={onClick} to={to}>
-      <EditIcon height={16} />
-      <Text>Edit</Text>
-    </Container>
-  );
+    key={post.slug}
+    showShareLinks={false}
+  />
+);
+const renderPaginationElements = () => {
+  const { LoadMore, Loader } = InfiniteList;
+  return [
+    <LoadMore key="more">
+      <LoadMoreButton>Load More Posts</LoadMoreButton>
+    </LoadMore>,
+    <Loader key="loader">
+      <LoaderButton>
+        <LoaderIcon />
+      </LoaderButton>
+    </Loader>
+  ];
 };
-
-class Contributions extends React.Component {
-  componentWillMount() {
-    this.props.fetchDrafts();
-    this.props.fetchUserPublishedPosts();
-  }
-  renderPost = post => (
-    <ListItem
-      {...post}
-      headerComponent={
-        <ListItem.Header.Container>
-          <ListItem.Header.CreatedOn timeStamp={post.createdOn} />
-          <ListItem.Separator delimiter="|" space={8} />
-          <ListItem.Header.TimeToRead ttr={post.ttr} />
-          <EditButton to={`write/${post.slug}`} />
-        </ListItem.Header.Container>
-      }
-      key={post.slug}
-      showShareLinks={false}
-    />
-  );
-  renderPublishedPosts = () => {
-    const { published = [] } = this.props.contributions || {};
-    return published.map(this.renderPost);
-  };
-  renderDrafts = () => {
-    const { drafts = [] } = this.props.contributions || {};
-    return drafts.map(this.renderPost);
-  };
-  render() {
-    return (
-      <Container>
-        <List>
-          <Heading>Drafts</Heading>
-          {this.renderDrafts()}
-          <Heading>Published Posts</Heading>
-          {this.renderPublishedPosts()}
-        </List>
-      </Container>
-    );
-  }
-}
+const Contributions = props => (
+  <Container>
+    <List>
+      <Heading>Drafts</Heading>
+      <InfiniteList loadMore={props.fetchDrafts}>
+        {props.contributions.drafts.map(renderPost)}
+        {renderPaginationElements()}
+      </InfiniteList>
+      <Heading>Published Posts</Heading>
+      <InfiniteList loadMore={props.fetchUserPublishedPosts}>
+        {props.contributions.published.map(renderPost)}
+        {renderPaginationElements()}
+      </InfiniteList>
+    </List>
+  </Container>
+);
 
 const mapStateToProps = ({ user, contributions }) => ({ user, contributions });
 const mapDispatchToProps = { fetchDrafts, fetchUserPublishedPosts };
---
+++ src/components/Contributions/styled.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import PenIcon from '../icons/Pen';
+import { Button } from '../common';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+export const List = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 16px;
+`;
+export const Heading = styled.h2`
+  font-size: 24px;
+  font-family: roboto;
+  font-weight: 300;
+  color: #f78d3d;
+  padding-bottom: 12px;
+  border-bottom: 1px solid #efefef;
+`;
+export const LoadMoreButton = Button.extend`
+  text-align: center;
+  border-color: #bababa;
+  width: 100%;
+  color: #444;
+  &:hover {
+    background-color: #f9f9f9;
+    color: #111;
+  }
+`;
+export const LoaderButton = LoadMoreButton.extend`
+  background-color: #f9f9f9;
+  cursor: wait;
+`;
+
+export const EditButton = ({ onClick, to }) => {
+  const Text = styled.span`
+    margin-left: 4px;
+  `;
+  const EditIcon = styled(PenIcon)`
+    fill: #999;
+  `;
+  const Container = styled(Link)`
+    cursor: pointer;
+    display: flex;
+    font-size: 16px;
+    color: #999;
+    margin-left: auto;
+    text-decoration: none;
+    &:hover {
+      ${EditIcon} svg {
+        fill: #777;
+      }
+      ${Text} {
+        color: #777;
+      }
+    }
+  `;
+  return (
+    <Container onClick={onClick} to={to}>
+      <EditIcon height={16} />
+      <Text>Edit</Text>
+    </Container>
+  );
+};
