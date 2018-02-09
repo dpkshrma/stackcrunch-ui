b4313998d9aa1c370d1cf3a2419643039524c1ce
Deepak Sharma <dpkshrma01@gmail.com>
2018-02-09 15:31:07 +0530

    Remove unnecessary formatting of post list data items

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -17,11 +17,16 @@ import {
   tagCSS,
   authorCSS
 } from './styled';
-import { TWITTER_SHARE_URL, STACKCRUNCH_POST_URL } from '../../config';
+import {
+  TWITTER_SHARE_URL,
+  STACKCRUNCH_POST_URL,
+  URL_PREFIX
+} from '../../config';
 import TwitterIcon from './icons/TwitterIcon';
 
-const Author = ({ profile: { img, name, link } }) => {
-  return <Chip img={img} text={name} to={link} css={authorCSS} />;
+const Author = ({ profile: { avatarURL, name, username } }) => {
+  const link = `${URL_PREFIX}/@${username}`;
+  return <Chip img={avatarURL} text={name} to={link} css={authorCSS} />;
 };
 const CreatedOn = ({ timeStamp }) => {
   return <HeaderMetaText>{fromNow(timeStamp)}</HeaderMetaText>;
@@ -35,7 +40,7 @@ const ListItem = ({
   title,
   slug,
   abstract,
-  author = {},
+  authors,
   tags,
   createdOn,
   ttr,
@@ -46,9 +51,10 @@ const ListItem = ({
   const tagList =
     tags &&
     tags.map((tag, i) => (
-      <Chip key={i} text={tag.text} to={tag.link} css={tagCSS} />
+      <Chip key={i} text={tag.name} to={tag.link} css={tagCSS} />
     ));
 
+  const [author] = authors;
   // TODO: ellipsify shareText if char length overshot
   const shareText = `${title} by @${author.twitterHandle}`;
   const shareLink = `${TWITTER_SHARE_URL}?text=${shareText}&url=${STACKCRUNCH_POST_URL}/${id}`;
@@ -61,6 +67,8 @@ const ListItem = ({
   if (headerComponent) {
     header = headerComponent;
   } else {
+    // FIXME: display owner instead of authors
+    // currently assuming only one author(owner)
     header = (
       <Header>
         <Author profile={author} />
