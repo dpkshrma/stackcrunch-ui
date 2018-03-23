be3394b68ade2eec2bec427e1b21dde7855fbc6e
Deepak Sharma <dpkshrma01@gmail.com>
2018-03-23 17:08:09 +0530

    Throw error for a non 2xx/3xx HTTP response

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,4 +1,5 @@
 import queryString from 'query-string';
+import { HTTPError } from './error';
 import { STACKCRUNCH_API_URL, STACKCRUNCH_TOKEN_ID } from '../config';
 
 export const jsonHeaders = {
@@ -8,11 +9,25 @@ export const jsonHeaders = {
 
 const fetchJSON = (url, opts) => {
   return fetch(url, opts)
-    .then(response => response.json())
-    .catch(err => {
-      console.error(err);
-      throw err;
-    });
+    .then(async response => {
+      const { status, statusText } = response;
+
+      const responseJSON = await response.json();
+      const { message } = responseJSON;
+
+      const statusType = status / 100;
+
+      if (statusType !== 2 || statusType !== 3) {
+        throw new HTTPError({
+          name: statusText,
+          message,
+          status
+        });
+      }
+
+      return responseJSON;
+    })
+    .then(response => response.json());
 };
 
 export const req = (path, { headers = {}, auth = true } = {}) => {
